ed879aee701f9ac9f17adf51992d6523010b892a
Felipeceu21 <juanceur@unisabana.edu.co>
2024-02-26 02:01:06 -0500

    to do list terminada

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-console.log("JavaScript working!");
+let addToDoButton = document.getElementById("btnAdd");
+let toDoContainer = document.getElementById("toDoContainer");
+let inputField = document.getElementById("inpField")
+
+addToDoButton.addEventListener("click",function(){
+    var paragraph = document.createElement("p");
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = " ";
+    
+    paragraph.addEventListener("click", function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener("dblclick",function(){
+        toDoContainer.removeChild(paragraph);
+    })
+
+});
+
